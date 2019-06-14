37176ab2142ad5682655e11ffbafc2f2d6f90352
chaoticgd <chaoticgd@protonmail.com>
2019-06-14 04:45:51 +0100

    Initial commit

---
+++ example.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+
+#include "refolder.h"
+
+struct person {
+
+	std::string name;
+	int age;
+	float height;
+
+	float get_height() { return height; }
+	void set_height(float h) {
+		if(h >= 0) {
+			height = h;
+		} else {
+			std::cout << "A person's height cannot be negative.\n";
+		}
+	}
+
+	template <typename... T>
+	void reflect(T... callbacks) {
+		rf::reflector r(this, callbacks...);
+		r.visit_r("First Name", name);
+		r.visit_f("Age",
+			[=]() { return age; },
+			[=](int a) { age = a; });
+		r.visit_m("Height", &person::get_height, &person::set_height);
+	}
+};
+
+int main() {
+	person p;
+	p.name = "John Smith";
+	p.age = 42;
+	p.height = 1.8;
+	p.reflect(
+		[](const char* name, rf::property<int> p) {
+			std::cout << "Property \"" << name << "\" of type int with value \"" << p.get() << "\".\n";
+		},
+		[](const char* name, rf::property<float> p) {
+			std::cout << "Property \"" << name << "\" of type float with value \"" << p.get() << "\".\n";
+		},
+		[](const char* name, rf::property<std::string> p) {
+			std::cout << "Property \"" << name << "\" of type string with string \"" << p.get() << "\".\n";
+		}
+	);
+}
---
+++ refolder.h
@@ -0,0 +1,99 @@
+/*
+	Copyright (c) 2019 Thomas Evans
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#ifndef LIB_REFOLDER_H
+#define LIB_REFOLDER_H
+
+#include <tuple>
+#include <functional>
+
+namespace rf {
+	template <typename T_value>
+	struct property {
+		std::function<T_value()> get;
+		std::function<void(T_value)> set;
+	};
+
+	template <typename T_object, typename... T_callbacks>
+	class reflector {
+	public:
+		reflector(T_object* that, T_callbacks... callbacks)
+			: _that(that), _callbacks(callbacks...) {}
+
+		template <typename T_value>
+		void visit_r(const char* name, T_value& value) {
+			// Wrap the reference in a pair of getters/setters.
+			visit_f(name,
+				[&value]() { return value; },
+				[&value](T_value v) { value = v; });
+		}
+
+		template <typename T_getter, typename T_setter>
+			// where T_getter, T_setter are member function pointers.
+		void visit_m(const char* name, T_getter get, T_setter set) {
+			// Convert the pair of getters/setters to a functor.
+			visit_f(name,
+				[=]() { return (*_that.*get)(); },
+				[=](decltype((*_that.*get)()) value)
+					{ (*_that.*set)(value); });
+		}
+
+		template <typename T_getter, typename T_setter>
+			// where T_getter, T_setter are functors.
+		void visit_f(const char* name, T_getter get, T_setter set) {
+			visit_n<T_getter, T_setter, 0>(name, get, set);
+		}
+
+	private:
+		template <typename T_getter, typename T_setter, std::size_t T_index>
+		void visit_n(const char* name, T_getter get, T_setter set) {
+			call(name,
+				std::function(get),
+				std::function(set),
+				std::function(std::get<T_index>(_callbacks)));
+			if constexpr(std::tuple_size_v<decltype(_callbacks)> > T_index + 1) {
+				visit_n<T_getter, T_setter, T_index + 1>(name, get, set);
+			}
+		}
+
+		template <typename T_value>
+		void call(
+			const char* name,
+			std::function<T_value()> get,
+			std::function<void(T_value)> set,
+			std::function<void(const char*, property<T_value>)> callback) {
+			
+			using value_type = decltype(get());
+			property<value_type> p { get, set };
+			callback(name, p);
+		}
+
+		// SFINAE fallthrough.
+		template <typename T_getter, typename T_setter, typename T_callback>
+		void call(const char* name, T_getter get, T_setter set, T_callback callback) {}
+
+		T_object* _that;
+		std::tuple<T_callbacks...> _callbacks;
+	};
+}
+
+#endif
